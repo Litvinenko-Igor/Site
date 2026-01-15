71005070086c2884e653e6cb8d0453bdb391ae98
Ihor Lytvynenko <litvinenkoigor437@gmail.com>
2026-01-15 06:00:24 +0100

    main Class

--- src/main/java/com/example/demo/Demo3Application.java
+++ src/main/java/com/example/demo/Demo3Application.java
@@ -13,36 +13,3 @@ public class Demo3Application {
 
     }
 }
-
-
-//public class HikariCPDataSource {
-//
-//    private static HikariConfig config = new HikariConfig();
-//    private static HikariDataSource ds;
-//
-//    static {
-//        config.setJdbcUrl("jdbc:h2:mem:test");
-//        config.setUsername("user");
-//        config.setPassword("password");
-//        config.addDataSourceProperty("cachePrepStmts", "true");
-//        config.addDataSourceProperty("prepStmtCacheSize", "250");
-//        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
-//        ds = new HikariDataSource(config);
-//    }
-//
-//    public static Connection getConnection() throws SQLException {
-//        return ds.getConnection();
-//    }
-//
-//    private HikariCPDataSource(){}
-
-
-//Properties properties = new Properties();
-//properties.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
-//properties.put(Environment.URL, "jdbc:mysql://localhost:3306/supershop") ;
-//properties.put(Environment.USER, "root");
-//properties.put(Environment.PASS, "password");
-//
-//SessionFactory sessionFactory = new Configuration()
-//        .setProperties(properties)
-//        .buildSessionFactory();
